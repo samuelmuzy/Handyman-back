cf95cf11057a749b3815eaa140f893bc29346676
samuel muzy <samuelmuzyfernandes4@gmail.com>
2025-04-13 19:54:05 -0300

    Criação da rota fornecedor no index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,6 @@
 import { app } from './app';
+import { fornecedorRouter } from './routes/fornecedor/FornecedorRoutes';
 import { usuarioRouter } from './routes/usuario/UsuarioRoutes';
 
 app.use('/usuarios',usuarioRouter);
+app.use('/fornecedor',fornecedorRouter);
