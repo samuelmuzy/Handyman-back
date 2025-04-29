df2851cf14656ee8535eec2753252d2ca26a1ca5
JoaoPauloAAlvim <alvesalvimj@gmail.com>
2025-04-29 14:00:09 -0300

    Mudança nas rotas

--- src/routes/fornecedor/FornecedorRoutes.ts
+++ src/routes/fornecedor/FornecedorRoutes.ts
@@ -7,7 +7,7 @@ const fornecedorController = new FornecedorController();
 
 // Rotas públicas
 fornecedorRouter.post('/', fornecedorController.criarFornecedor);
-fornecedorRouter.post('/login', fornecedorController.login);
+fornecedorRouter.post('/login-fornecedor', fornecedorController.login);
 
 // Rotas protegidas
 fornecedorRouter.get('/', fornecedorController.buscarFornecedores);
